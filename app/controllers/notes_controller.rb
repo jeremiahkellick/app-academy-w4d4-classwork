d27f8cd982b9b470e8e914707a0a1cd32e7117d1
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2018-09-13 23:03:27 -0700

    Add note form

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,31 @@
 class NotesController < ApplicationController
+  before_action :must_be_logged_in
+
   def create
+    @note = Note.new(note_params)
+    @note.user = current_user
+    @note.track_id = params[:track_id]
+    if @note.save
+      redirect_to track_url(params[:track_id])
+    else
+      flash[:errors] = @note.errors.full_messages
+      redirect_to track_url(params[:track_id])
+    end
   end
 
   def destroy
+    @note = Note.find(params[:id])
+    if @note.user.id == current_user.id
+      @note.destroy
+      redirect_to track_url(@note.track)
+    else
+      render plain: "403 Forbidden", status: 403
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:body)
   end
 end
--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,9 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    (
+      '<pre>' +
+      @track.lyrics.split("\n").map { |line| "&#9835; #{h(line)}" }.join("\n") +
+      '</pre>'
+    ).html_safe
+  end
 end
